795fd5c1be9f841257550ddf01ef090e36f8abae
SooLight <soso1461@naver.com>
2023-09-19 12:49:40 +0900

    Read 까지 작성

--- src/main/java/com/soo/basic/repository/UserRepository.java
+++ src/main/java/com/soo/basic/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.soo.basic.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.soo.basic.entity.UserEntity;
@@ -12,5 +15,44 @@ import com.soo.basic.entity.UserEntity;
 // description: JpaRepository 인터페이스 - JPA 기반의 Repository 인터페이스를 구현하는데 사용되는 인터페이스 //
 // description: JpaRepository<T, ID> - T: 해당 리포티토리에서 사용될 엔터티 클래스, ID: 해당 엔터티 클래스에서 지정한 기본키 필드의 타입  //
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    // SELECT * FROM user WHERE email = '??';
+    UserEntity findByEmail(String email);
+    // SELECT * FROM user WHERE email = '??' AND nickname = '??';
+    UserEntity findByEmailAndNickname(String email, String nickname);
+    // SELECT * FROM user WHERE address_detail = '??' ORDER BY address DESC;
+    List<UserEntity> findByAddressDetailOrderByAddressDesc(String addressDetail);
+
+    boolean existsByEmail(String email);
+    // SELECT * FROM user WHERE email = '?' OR nickname = '?' OR tel_number = '?' -> 레코드가 존재하는지
+    boolean existsByEmailOrNicknameOrTelNumber(String email, String nickname, String telNumber);
+
+    long countByAddress(String address);
+
+    // description: @Query - 쿼리 메서드 만으로 데이터베이스 작업에 수행할 수 없을 때 사용하는 어노테이션 //
+    // description: JPQL (Java Persistence Query Language) - SQL과 문법은 유사하지만 데이터베이스 테이블이 아닌 엔터티 클래스 기준으로 쿼리를 작성 //
+    // description: Native Query - SQL  //
     
+    // JPQL
+    // SELECT * FROM user WHERE email = '??';
+    @Query(value = "SELECT u FROM user u WHERE u.email = ?1")
+    UserEntity findByEmailJPQL(String email);
+    // SQL
+    @Query(value = "SELECT * FROM user WHERE email = ?1", nativeQuery=true)
+    UserEntity findByEmailSQL(String email);
+
+    // SELECT * FROM user WHERE email = '??' AND nickname = '??';
+    // JPQL
+    @Query(value = "SELECT u FROM user u WHERE u.email = ?1 AND u.nickname =?2")
+    UserEntity findByEmailAndNicknameJPQL(String email, String nickname);
+    // SQL ex)
+    @Query(value = 
+    "SELECT * " +       // 이렇게 쓸 때 뒤에 띄어쓰기 무조건 넣어야하고 가독성을 위해 이렇게 작성해도 됨
+    "FROM user " +
+    "WHERE email IN( " +
+    "   SELECT DISTINCT writer_email " +
+    "   FROM board " +
+    ")"
+    , nativeQuery = true)
+    List<UserEntity> getBoardWriterUserList();
 }
